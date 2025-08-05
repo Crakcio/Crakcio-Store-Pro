2abb9218a0569b2cd4e9ab5c22709396a481d552
Crakcio <150481193+Crakcio@users.noreply.github.com>
2025-08-05 15:38:56 -0500

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -4,3 +4,5 @@ const supabaseUrl = 'https://mzkftygjnkosmdhkvquf.supabase.co'; // Reemplaza con
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16a2Z0eWdqbmtvc21kaGt2cXVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1OTEzNzMsImV4cCI6MjA2OTE2NzM3M30.2N5YK74lat6howsKk4RTJqkYU1CPcuuOWTcVtjTZn0s'; // Reemplaza con tu clave pública
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
